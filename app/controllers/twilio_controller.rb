44ad60b1c112ded63d325d77a0d98c76dce98b58
Tyler Harpool <tdhz77@mizzou.edu>
2014-04-11 09:53:02 +0000

    Modifield Twiliocontroller

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -3,25 +3,24 @@ class TwilioController < ApplicationController
   ACCOUNT_SID = 'AC88068d1ee49052acdc5a8bc1e1ef8b51'
   ACCOUNT_TOKEN = '3029f8f189ab401f6f22961317f4dc5e'
   
-  
+  include Webhookable
  
   after_filter :set_header
  
   def voice
-     @game = Game.last
+    @game = Game.first
     response = Twilio::TwiML::Response.new do |r|
-      r.Say "Our next game will be on field #{@game.field}. The Game will start at #{@game.start_at}. We are going to beat the shit out of #{@game.opponent}", :voice => 'alice'
+      r.Say "Our next game will be on field #{@games.field} at #{@game.start_at}. We are going to beat the shit out of #{@game.opponent}", :voice => 'alice'
          r.Play 'http://linode.rabasa.com/cantina.mp3'
       r.Dial '14178802516'
     end
  
     render_twiml response
   end
-  
 
 
   def sms
-    @game = Game.last
+    @game = Game.first
     render 'sms.xml.erb', :content_type => 'text/xml'
   end
 
