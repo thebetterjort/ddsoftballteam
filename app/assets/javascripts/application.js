040e4871244a0acd1fc310f2d8a6a1dcc0730f4c
Tyler Harpool <tdhz77@mizzou.edu>
2014-04-14 21:53:04 +0000

    Fixed voice changes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,13 @@
 //= require turbolinks
 //= require bootstrap
 //= require_tree .
+/* off-canvas sidebar toggle */
+
+$('[data-toggle=offcanvas]').click(function() {
+  	$(this).toggleClass('visible-xs text-center');
+    $(this).find('i').toggleClass('glyphicon-chevron-right glyphicon-chevron-left');
+    $('.row-offcanvas').toggleClass('active');
+    $('#lg-menu').toggleClass('hidden-xs').toggleClass('visible-xs');
+    $('#xs-menu').toggleClass('visible-xs').toggleClass('hidden-xs');
+    $('#btnShow').toggle();
+});
